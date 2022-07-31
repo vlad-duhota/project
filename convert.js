c6b70022bac642d35aa33514e81dc132f75fe989
vlad-duhota <vladarduino@gmail.com>
2022-07-31 19:27:42 +0300

    convert 2

--- convert.js
+++ convert.js
@@ -4,7 +4,7 @@ const toInput = document.querySelector('#toInput');
 form.addEventListener('submit', function (e) { 
     e.preventDefault(); 
     const formData = new FormData(e.target); 
-    axios(`https://v2.convertapi.com/convert/${fromInput.value}/to/${toInput.value}?Token=DG7evB9j`, { 
+    axios(`https://v2.convertapi.com/convert/${formData.get('from')}/to/${toInput.value}?Token=DG7evB9j`, { 
         method: 'POST', 
         data: formData, 
     }) 
@@ -12,8 +12,8 @@ form.addEventListener('submit', function (e) {
         imgCode = data.data.Files[0].FileData; 
         const img = document.querySelector('#img');
         const link = document.querySelector('#link');
-        img.src = 'data:image/png;base64,' + imgCode;
+        img.src = `data:image/${toInput.value};base64,` + imgCode;
         link.style.display = 'block';
-        link.href = 'data:image/png;base64,' + imgCode;
+        link.href = `data:image/${toInput.value};base64,` + imgCode;
     }) 
 });
